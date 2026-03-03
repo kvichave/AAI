2ac9cb58218cf4c6736f3acba49492821beb83f0
kunalvichave <kunalvichave258@gmail.com>
2026-03-03 23:38:57 +0530

    adding tools using json

--- MCP/weather.py
+++ MCP/weather.py
@@ -1,11 +1,25 @@
 from mcp.server.fastmcp import FastMCP
-
+from langchain_tavily import TavilySearch
+from langchain_community.tools.tavily_search import TavilySearchResults
+from dotenv import load_dotenv
+import json
+import os
+load_dotenv()
 mcp = FastMCP("Weather")
 
 @mcp.tool()
 async def get_weather(city: str) -> str:
     """Get weather of a city"""
     return f"Weather in {city} is sunny"
+
+
+@mcp.tool()
+async def search_web(query:str)->str:
+    """Search the web for information."""
+
+    search = TavilySearchResults(api_key=os.getenv("TAVILY_API_KEY"),max_results=3)
+    results=await search.ainvoke(query)
+    return str(results)
 
 
 if __name__ == "__main__":
