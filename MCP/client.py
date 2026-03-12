06253fec1032ac3ed79f5bb80172afced381c976
kunal <kunalvichave258@gmail.com>
2026-03-12 16:04:28 +0530

    creating agentg with database mcp

--- MCP/client.py
+++ MCP/client.py
@@ -1,22 +1,33 @@
-from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
-
-import asyncio
-
-from dotenv import load_dotenv
-import json
-load_dotenv()
 import os
+import importlib.util
+from langchain_core.tools import Tool
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START
+from langgraph.prebuilt import ToolNode, tools_condition
+from typing import TypedDict, Annotated
+from langchain_core.messages import HumanMessage
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+load_dotenv()
+
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
 
 
 
 llm = ChatOpenAI(
     model="qwen/qwen3-32b",
-    base_url=os.getenv("GROQ_URL"),
+    base_url="https://api.groq.com/openai/v1",
     temperature=0.7,
-    api_key=os.getenv("GROQ_API"),
+    api_key=os.getenv("GROQ_API_KEY"),
 )
+
+def chatbot(state: State):
+
+    response = llm.invoke(state["messages"])
+
+    return {"messages": [response]}
 
 async def main():
     mcp_client = MultiServerMCPClient(
@@ -31,9 +42,11 @@ async def main():
     except Exception as e:
         print("Failed to load MCP tools:", e)
         return
-    agent=create_agent(llm,tools)
-    weather_response=await agent.ainvoke({"messages":[{"role":"user","content":"What is the latest news, websearch it"}]})
-    print(weather_response["messages"][-1].content)
+
+    
+    # agent=create_agent(llm,tools)
+    # weather_response=await agent.ainvoke({"messages":[{"role":"user","content":"what is "}]})
+    # print(weather_response["messages"][-1].content)
 
 
 asyncio.run(main())
